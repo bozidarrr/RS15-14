f51ecd7faa1cf863e960a4afc079f4954a16e2e5
Bozidar <bozidarrr@gmail.com>
2015-05-04 00:35:24 +0200

    Napravio povezivanje dve osobe, za sada deluje da engine ne pravi problem. Moracu sutra da precistim kod ujutru, pre nego sto neko drugi krene da ga koristi, da bih stvari nazvao pravim imenom. Odusevljeno sam saznao da mi ipak treba drag and drop, posto u momentu kada jedan widget uzme mousepressed on drzi i misa i samo on moze da prihvati mouse released. Glup jezik

--- ProjectAlmond/GUI/widgetosoba.cpp
+++ ProjectAlmond/GUI/widgetosoba.cpp
@@ -2,8 +2,8 @@
 #include "ui_widgetosoba.h"
 #include "GUI/glavniprozor.h"
 #include <iostream>
-
-WidgetOsoba::WidgetOsoba(short int sifra, int x, int y, const QString &ime, const QString &prezime, GlavniProzor2 *gp, QWidget *parent) :
+#include <QtGui>
+WidgetOsoba::WidgetOsoba(short int sifra, int x, int y,  GlavniProzor2 *gp, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::WidgetOsoba),
     _sifra(sifra),_x(x),_y(y),
@@ -11,7 +11,7 @@ WidgetOsoba::WidgetOsoba(short int sifra, int x, int y, const QString &ime, cons
 {
     ui->setupUi(this);
     //ovako se menja izgled pomocu css-a
-    ui->btnOsoba->setStyleSheet (" background-color:rgb(128,255,128);"
+    ui->ImeOsobe->setStyleSheet (" background-color:rgb(128,255,128);"
                                  "color: blue;"
                                  "font-style:italic;"
                                  "selection-background-color: rgb(128,255,128);"
@@ -21,11 +21,10 @@ WidgetOsoba::WidgetOsoba(short int sifra, int x, int y, const QString &ime, cons
     //sad kad sam nasla kako se to implementira mozda bi hteli malo drugacije da napravimo widget posto sam trazila i ne postoji
     //toolkit u Qt mislila sam mozda da dodamo neku labelu sa zaobljenim ivicama posto u css-u to moze da se kaze
 
-//    connect(ui->btnOsoba,SIGNAL(pressed()),this,SLOT(on_btnOsoba_pressed()));
-//       connect (ui->btnOsoba,SIGNAL(clicked()),this,SLOT(on_btnOsoba_clicked()));
-//       connect(ui->btnOsoba,SIGNAL(released()),this,SLOT(on_btnOsoba_released()));
-//       connect(ui->btnOsoba,SIGNAL(toggled(bool)),this,SLOT(on_btnOsoba_toggled(bool)));
+
 }
+
+
 
 WidgetOsoba::~WidgetOsoba()
 {
@@ -48,9 +47,9 @@ short int WidgetOsoba::Sifra()const
 bool WidgetOsoba::sadrziTacku(int x, int y)
 {
     bool rez = x >= _x
-            && x <= _x + ui->btnOsoba->width()
+            && x <= _x + ui->ImeOsobe->width()
             && y >= _y
-            && y <= _y + ui->btnOsoba->height();
+            && y <= _y + ui->ImeOsobe->height();
     return rez;
 
 }
@@ -58,10 +57,54 @@ bool WidgetOsoba::sadrziTacku(int x, int y)
 void WidgetOsoba::postaviImePrezime(const std::string &ip)
 {
     QString q_ip = QString::fromStdString(ip);
-    ui->btnOsoba->setText(q_ip);
+    ui->ImeOsobe->setText(q_ip);
+}
+
+void WidgetOsoba::mousePressEvent(QMouseEvent *event)
+{
+
+      std::cout<<"pritisao misa na osobu "<<_sifra<<std::endl;
+    if (event->button() == Qt::LeftButton) {
+        QDrag *drag = new QDrag(this);
+              QMimeData *mimeData = new QMimeData;
+              mimeData->setText(QString(_sifra));
+              drag->setMimeData(mimeData);
+              w->postaviSifru1(_sifra);
+              drag->exec();
+
+
+    }
+
+}
+
+void WidgetOsoba::dragEnterEvent(QDragEnterEvent *event)
+{
+           event->acceptProposedAction();
 }
 
 
+void WidgetOsoba::dropEvent(QDropEvent *event)
+{
+  w->postaviSifru2(_sifra);
+  w->otpustenaOsoba();
+    event->acceptProposedAction();
+
+}
+
+
+void WidgetOsoba::mouseReleaseEvent(QMouseEvent *event)
+{
+    std::cout<<"pustio misa sa osobe "<<_sifra<<std::endl;
+ /*   if (event->button() == Qt::LeftButton) {
+  //     emit otpusten();
+    }*/
+
+}
+
+
+
+
+/*
 void WidgetOsoba::on_btnOsoba_clicked()
 {
     if (w->Povezivati())
@@ -82,14 +125,14 @@ void WidgetOsoba::on_btnOsoba_clicked()
         w->popuniInformacije();
     }
 }
-
+*//*
 void WidgetOsoba::on_btnOsoba_pressed()
 {
     //pocinjemo povezivanje sa drugom osobom - tj kreiranje relacije u zavisnosti od radio buttona
     //nece moci ovako jer se ovo desava i na clicked :P
    // w->postaviSifru1(_sifra);
 }
-
+*//*
 void WidgetOsoba::on_btnOsoba_released()
 {
     //a na ovo kreiramo relaciju ako je sve bilo ok
@@ -100,7 +143,7 @@ void WidgetOsoba::on_btnOsoba_released()
 
    // w->postaviSifru2(_sifra);
    // w->poveziOsobe();
-}
+}*/
 //bool WidgetOsoba::operator ==(const WidgetOsoba& druga)const{
 //    //za proveru jednakosti 2 widgeta dovoljno je da proverimo njihove sifre jer je to jedinstveni identifikator
 //    if(_sifra==druga._sifra)
